0a991792d39ea1cac82ae25f398a1a13ffd4587a
Eddie Bonk <eddie@heysparkbox.com>
2013-03-21 16:30:51 -0400

    Added test for diagonal wins, and the ability to determine if a space is occupied by a circle or an x

--- lib/tictac.rb
+++ lib/tictac.rb
@@ -8,7 +8,6 @@ class Cell
 	end
 end
 
-
 class Game 
 	attr_accessor :squares
 
@@ -21,44 +20,110 @@ class Game
 		 @squares << cell
 	end
 
-	def test_vertical
-		col1 = 0
-		col2 = 0
-		col3 = 0
+	def test_vertical_win?
+		col1_x = 0
+		col2_x = 0
+		col3_x = 0
+		col1_o = 0
+		col2_o = 0
+		col3_o = 0
+
 		squares.each do |cell| 
-			if lane1(cell.y)
-				col1 += 1
+			if lane1(cell.y) && state_o?(cell.value)
+				col1_o += 1
 			end
-			if lane2(cell.y)
-				col2 += 1
+			if lane1(cell.y) && state_x?(cell.value)
+				col1_x += 1
 			end
-			if lane3(cell.y)
-				col3+= 1
+			if lane2(cell.y) && state_o?(cell.value)
+				col2_o += 1
+			end
+			if lane2(cell.y) && state_x?(cell.value)
+				col2_x += 1
+			end
+			if lane3(cell.y) && state_o?(cell.value)
+				col3_o += 1
+			end
+			if lane3(cell.y) && state_x?(cell.value)
+				col3_x += 1
 			end
 		end
-		if col1 == 3 || col2 == 3 || col3 == 3
-			return 3
+		if col1_x == 3 || col2_x == 3 || col3_x == 3
+			return true
+		elsif col1_o == 3 || col2_o == 3 || col3_o == 3
+			return true
+		else
+			false
+		end 
+	end
+
+	def test_horizontal_win?
+		col1_x = 0
+		col2_x = 0
+		col3_x = 0
+		col1_o = 0
+		col2_o = 0
+		col3_o = 0
+
+		squares.each do |cell| 
+			if lane1(cell.x) && state_o?(cell.value)
+				col1_o += 1
+			end
+			if lane1(cell.x) && state_x?(cell.value)
+				col1_x += 1
+			end
+			if lane2(cell.x) && state_o?(cell.value)
+				col2_o += 1
+			end
+			if lane2(cell.x) && state_x?(cell.value)
+				col2_x += 1
+			end
+			if lane3(cell.x) && state_o?(cell.value)
+				col3_o += 1
+			end
+			if lane3(cell.x) && state_x?(cell.value)
+				col3_x += 1
+			end
+		end
+		if col1_x == 3 || col2_x == 3 || col3_x == 3
+			return true
+		elsif col1_o == 3 || col2_o == 3 || col3_o == 3
+			return true
+		else
+			false
 		end
 	end
 
-	def test_horizontal
-		col1 = 0
-		col2 = 0
-		col3 = 0
-		squares.each do |cell| 
-			if lane1(cell.x)
-				col1 += 1
+	def test_diagonal_win?
+		total_x = 0
+		total_o = 0
+		squares.each do |cell|
+			if diagonal_square?(cell.x, cell.y) && state_o?(cell.value)
+				total_o += 1
 			end
-			if lane2(cell.x)
-				col2 += 1
-			end
-			if lane3(cell.x)
-				col3+= 1
+			if diagonal_square?(cell.x, cell.y) && state_x?(cell.value)
+				total_x += 1
 			end
 		end
-		if col1 == 3 || col2 == 3 || col3 == 3
-			return 3
+		if total_o == 3
+			return true
+		elsif total_x == 3
+			return true
+		else
+			false
 		end
+	end
+
+	def state_o?(cell_o)
+		cell_o == 'o'
+	end
+
+	def state_x?(cell_x)
+		cell_x == 'x'
+	end
+
+	def diagonal_square?(cellx, celly)
+		cellx.abs == celly.abs
 	end
 
 	def lane1(cell)
--- spec/tictac_spec.rb
+++ spec/tictac_spec.rb
@@ -22,19 +22,59 @@ describe 'tictac' do
 		game.squares.count.should == 3
 	end
 
-	it 'should test for 3 vertical os' do
+	it "should test for 3 vertical o's" do
 		game = Game.new
 		game.square_marked(-1,0,'o')
 		game.square_marked(0,0,'o')
 		game.square_marked(1,0,'o')
-		game.test_vertical.should == 3
+		game.test_vertical_win?.should == true
 	end
 
-	it 'should test for 3 horizontal os' do 
+	it "should test for 3 horizontal o's" do 
 		game = Game.new
 		game.square_marked(0,-1,'o')
 		game.square_marked(0,0,'o')
 		game.square_marked(0,1,'o')
-		game.test_horizontal.should == 3
+		game.test_horizontal_win?.should == true
+	end
+
+	it "should test for 3 diagonal o's from bottom left to top right" do 
+		game = Game.new
+		game.square_marked(1,1,'o')
+		game.square_marked(0,0,'o')
+		game.square_marked(-1,-1,'o')
+		game.test_diagonal_win?.should == true
+	end
+
+	it "should test for 3 diagonal o's from bottom right to top left" do 
+		game = Game.new
+		game.square_marked(-1,1,'o')
+		game.square_marked(0,0,'o')
+		game.square_marked(1,-1,'o')
+		game.test_diagonal_win?.should == true
+	end
+
+	it "should not score a game as winning if there is a mix of x's and o's in a row" do
+		game = Game.new
+		game.square_marked(0,1,'o')
+		game.square_marked(0,0,'x')
+		game.square_marked(0,-1,'o')
+		game.test_horizontal_win?.should == false
+	end
+
+	it "should not score a game as winning if there is a mix of x's and o's in a column" do 
+		game = Game.new
+		game.square_marked(1,0,'o')
+		game.square_marked(0,0,'x')
+		game.square_marked(-1,0,'o')
+		game.test_horizontal_win?.should == false
+	end
+
+	it "should not score a game as winning if there is a mix of x's and o's in a diagonal" do 
+		game = Game.new
+		game.square_marked(1,1,'o')
+		game.square_marked(0,0,'x')
+		game.square_marked(-1,-1,'o')
+		game.test_diagonal_win?.should == false
 	end
 end
